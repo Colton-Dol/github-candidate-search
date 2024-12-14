df7c27170a5bc6ed5ca82e9fda04e5e18e73a5f3
Colton Doling <cmdoling@gmail.com>
2024-12-14 16:12:35 -0500

    created an interface for the Candidates

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    avatar_url: string,
+    name: string,
+    username: string,
+    location: string,
+    email: string,
+    company: string,
+    bio: string
+}
