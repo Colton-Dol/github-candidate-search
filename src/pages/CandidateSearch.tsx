5696378dcaeb4a58948dd7f962c4cdf32175794d
Colton Doling <cmdoling@gmail.com>
2024-12-20 15:10:03 -0500

    added temp code for displaying candidates

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,38 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+//import Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [candidates, setCandidates] = useState<any>([]);
+
+  const candidatesArray: any = [];
+  useEffect(() => {
+    setCandidates([{id: 1, name: 'oliver', email: 'olimax@gmail.com'}]);
+  }, [])
+
+  useEffect(() => {
+    searchGithub().then(data => {
+     searchGithubUser(data[0].login)
+     .then(user => {
+      candidatesArray.push(user);
+     })
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>Candidate Search</h1>
+      {candidates.map((candidate: any) => {
+        return (
+          <>
+           <p>{candidate.name}</p>
+           <p>{candidate.email}</p>
+          </>
+        )
+      })}
+      <p>{candidatesArray}</p>
+    </div>
+  );
 };
 
 export default CandidateSearch;
