e353a311e2d644bc9be972bd1aa62ff855f462c1
Colton Doling <cmdoling@gmail.com>
2025-01-16 14:28:59 -0500

    added conditional rendering to display a message when there are no saved candidates

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -20,33 +20,41 @@ const SavedCandidates = () => {
 
   return (
     <>
-      <h1>Potential Candidates</h1>
-      <table className="table">
-        <tr>
-          <th scope="col">Image</th>
-          <th scope="col">Name</th>
-          <th scope="col">Location</th>
-          <th scope="col">Email</th>
-          <th scope="col">Company</th>
-          <th scope="col">Bio</th>
-          <th scope="col">Reject</th>
-        </tr>
-        <tbody>
-          {candidates.map((candidate: any) => {
-            return (
-              <tr key={candidate.id}>
-                <th scope="row"><img src={candidate.avatar_url}/></th>
-                <td>{candidate.name}</td>
-                <td>{candidate.location}</td>
-                <td>{candidate.email}</td>
-                <td>{candidate.company}</td>
-                <td>{candidate.bio}</td>
-                <td><button onClick={() => reject(candidate.id)}>Button</button></td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {JSON.stringify(candidates) === '[]' ? (
+        <div style={{backgroundColor: 'black', borderRadius: 25, textAlign: 'center'}}>
+          <h2>No Candidates Saved</h2>
+        </div>
+      ) : (
+        <>
+          <h1>Potential Candidates</h1>
+          <table className="table">
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Location</th>
+              <th scope="col">Email</th>
+              <th scope="col">Company</th>
+              <th scope="col">Bio</th>
+              <th scope="col">Reject</th>
+            </tr>
+            <tbody>
+              {candidates.map((candidate: any) => {
+                return (
+                  <tr key={candidate.id}>
+                    <th scope="row"><img src={candidate.avatar_url}/></th>
+                    <td><h3>{candidate.name}</h3><h3>({candidate.username})</h3></td>
+                    <td>{candidate.location}</td>
+                    <td>{candidate.email}</td>
+                    <td>{candidate.company}</td>
+                    <td>{candidate.bio}</td>
+                    <td><button onClick={() => reject(candidate.id)}>Reject</button></td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+      </>
+      )}
     </>
   );
 };
